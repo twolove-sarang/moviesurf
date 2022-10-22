830dfd363d8177be49209e54bbe545ff9fe92ed7
sarang <twolovelove2232@gmail.com>
2022-10-22 11:42:27 +0900

    third commit

--- src/pages/MovieSurf.js
+++ src/pages/MovieSurf.js
@@ -2,17 +2,24 @@ import React from "react";
 import { movieAction } from "../redux/action/movieAction";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import Banner from "../component/Banner";
 
 const MovieSurf = () => {
   const dispatch = useDispatch();
-  const {currentMovie,upComingMovie,popularMovie} = useSelector(state => state.movie)
-  console.log("currentMovie",currentMovie)
+  const { currentMovie, upComingMovie, popularMovie } = useSelector(
+    (state) => state.movie
+  );
+  console.log("currentMovie", currentMovie);
 
   useEffect(() => {
     dispatch(movieAction.getMovie());
   }, []);
 
-  return <div>MovieSurf</div>;
+  return (
+    <div>
+      {currentMovie.results && <Banner movie={currentMovie.results[0]} />}
+    </div>
+  );
 };
 
 export default MovieSurf;
